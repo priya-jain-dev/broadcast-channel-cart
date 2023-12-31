017d54a3390a6e5f9b71d194a87a5aee31848741
Priya Jain <priya2030.info@gmail.com>
2023-12-31 14:43:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+let cartCount = 0;
+const addToCartBtn = document.getElementById("add-btn");
+const removeBtn = document.getElementById("remove-btn");
+const cartBadge = document.getElementById("cart-count");
+const box = document.getElementById("box");
+const channel = new BroadcastChannel("app-broadcast-channel");
+
+const updateCartDisplay = () => {
+  cartBadge.innerHTML = cartCount;
+};
+
+const handleBoxAnimation = (animationClass) => {
+  box.classList.add(animationClass);
+  setTimeout(() => {
+    box.classList.remove(animationClass);
+  }, 1000);
+};
+
+const sendMessage = (action, count) => {
+  const message = { action, count };
+  channel.postMessage(JSON.stringify(message));
+};
+
+addToCartBtn.addEventListener("click", () => {
+  handleBoxAnimation("add-box");
+  cartCount++;
+  sendMessage("add", cartCount);
+  setTimeout(updateCartDisplay, 500);
+});
+
+removeBtn.addEventListener("click", () => {
+  if (cartCount > 0) {
+    handleBoxAnimation("remove-box");
+    cartCount--;
+    sendMessage("remove", cartCount);
+    setTimeout(updateCartDisplay, 500);
+  }
+});
+
+channel.onmessage = (event) => {
+  console.log(`Received message: ${event.data}`);
+  const data = JSON.parse(event.data);
+  console.log(data.action);
+  if (data.action === "add" || data.action === "remove") {
+    data.action === "add" && handleBoxAnimation("add-box");
+    data.action === "remove" && handleBoxAnimation("remove-box");
+    cartCount = data.count;
+    setTimeout(updateCartDisplay, 500);
+  }
+};
